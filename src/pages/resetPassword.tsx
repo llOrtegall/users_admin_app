05d7975c5099726e4fa5b3f2afbf33e2612da8a1
llOrtegall <IvanOrtega_97@hotmail.com>
2025-02-26 11:38:49 -0500

    feat: Enhance reset password flow with step-based UI updates and improved messaging

--- src/pages/resetPassword.tsx
+++ src/pages/resetPassword.tsx
@@ -3,6 +3,13 @@ import { Toaster, toast } from 'sonner';
 import React, { useState } from 'react';
 import axios from 'axios';
 
+function Calculate(step: number, totalSteps: number = 4): string {
+  if (step < 1 || step > totalSteps) {
+    throw new Error(`Step must be between 1 and ${totalSteps}`);
+  }
+  return `calc(100% * ${step} / ${totalSteps})`;
+}
+
 export default function ResetPassword() {
   const [document, setDocument] = useState('');
   const [email, setEmail] = useState('');
@@ -31,10 +38,10 @@ export default function ResetPassword() {
     <div className='flex min-h-screen bg-gray-100 p-2 justify-center'>
 
       <div className='px-2'>
-        <div className='h-full w-2 overflow-hidden rounded-md bg-gray-300'>
+        <div className='h-full w-2 overflow-hidden rounded-md bg-gray-300 transition-all duration-300 ease-in-out'>
           <div
-            className='h-full bg-blue-600'
-            style={{ height: 'calc(100% * 1 / 3)' }}
+            className='h-full bg-blue-600 transition-all duration-500 ease-in-out'
+            style={{ height: `${Calculate(steps)}` }}
           >
           </div>
         </div>
@@ -43,7 +50,7 @@ export default function ResetPassword() {
       <div className='p-8 flex flex-col justify-center items-center pb-20'>
         {
           steps === 1 && (
-            <div className='bg-blue-200 px-8 py-4 border border-gray-200 rounded-lg'>
+            <div className='w-[450px] bg-slate-200 px-8 py-6 rounded-lg border border-gray-300 shadow-lg'>
               <h1 className='text-xl font-bold mb-4'>Recuperación Contraseña</h1>
               <form onSubmit={handleSubmit} className=''>
 
@@ -89,21 +96,42 @@ export default function ResetPassword() {
         }
         {
           steps === 2 && (
-            <div className='bg-blue-200 px-8 py-4 border border-gray-200 rounded-lg'>
+            <div className='w-[450px] bg-slate-200 px-8 py-6 rounded-lg border border-gray-300 shadow-lg'>
               <h1 className='text-xl font-bold mb-4'>Recuperación Contraseña</h1>
-              <p className='text-center'>Por favor revisa tu correo electronico para continuar con el proceso</p>
+              <p className=''>Por favor revisa tu correo electronico donde se recepcionará un código válido por 10 min para continuar el restablecimiento de contraseña.</p>
+
+
+              <button
+                onClick={() => setSteps(3)}
+                className='w-full bg-green-800 text-white py-2 px-4 rounded-md hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 mt-4'
+              >
+                Código Verificación Recibido
+              </button>
+
+              <p className='text-sm mt-4 text-center'>
+                <span className='font-bold'>Nota: </span>Si no recibes el correo en tu bandeja de entrada, por favor revisa la bandeja de spam o correo no deseado.
+
+              </p>
+
+
+              <button
+                onClick={() => setSteps(1)}
+                className='w-full bg-indigo-600 text-white py-2 px-4 rounded-md hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 mt-4'
+              >
+                Volver a Solcitar Código
+              </button>
             </div>
           )
         }
         {
           steps === 3 && (
-            <div className='bg-blue-200 px-8 py-4 border border-gray-200 rounded-lg'>
+            <div className='w-[450px] bg-slate-200 px-8 py-6 rounded-lg border border-gray-300 shadow-lg'>
               <h1 className='text-xl font-bold mb-4'>Recuperación Contraseña</h1>
               <form className=''>
 
                 <div className='mb-4'>
                   <label className='block text-sm font-medium text-gray-700'>
-                    Token de Verificación
+                    Token Verificación
                   </label>
                   <input
                     type='password'
@@ -153,10 +181,18 @@ export default function ResetPassword() {
             </div>
           )
         }
+        {
+          steps === 4 && (
+            <div className='w-[450px] bg-green-200 px-8 py-6 rounded-lg border border-gray-300 shadow-lg'>
+              <h1 className='text-xl font-bold mb-4'>Recuperación Contraseña</h1>
+              <p className=''>Tu contraseña ha sido cambiada exitosamente. ya puedes ingresar en los diferentes aplicativos en la cual estás credenciales sean requeridas.</p>
+            </div>
+          )
+        }
       </div>
 
       <Toaster
-        position='bottom-right'
+        position='top-right'
         visibleToasts={3}
         richColors
         closeButton
