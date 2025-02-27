0c2d0486e4c60b673879309920d5cf8261f189e6
llOrtegall <IvanOrtega_97@hotmail.com>
2025-02-27 08:34:45 -0500

    feat: Disable submit button during loading state in reset password component

--- src/pages/resetPassword.tsx
+++ src/pages/resetPassword.tsx
@@ -142,6 +142,7 @@ export default function ResetPassword() {
 
                 <button
                   type='submit'
+                  disabled={loader}
                   className='w-full bg-blue-600 text-white py-2 px-4 rounded-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2'
                 >
                   Enviar Solicitud
